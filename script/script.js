9022249e70b005a05feda0185998ff213dace5fd
clemchret <clement.chret@gmail.com>
2023-05-09 02:16:30 +0200

    Initial commit

---
+++ script/script.js
@@ -0,0 +1,8 @@
+const menuBurger = document.querySelector('.menu-burger');
+const menuItems = document.querySelector('.menu-items');
+
+menuBurger.addEventListener('click', function() {
+  menuBurger.classList.toggle('active');
+  menuItems.classList.toggle('active');
+});
+
